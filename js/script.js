17210fe22e6314fd636c735f7dcce96f544565c5
Orirogge <ori.roggemans@student.arteveldehs.be>
2017-06-09 16:33:18 +0200

    wip3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         document.getElementById("top_button").style.display = "block";
